2dd81c75c0d27b78ab539b9bc2ef16f273941153
mingthean.lay <mingthean.lay@gmail.com>
2022-07-22 00:58:23 +0700

    chore: remove unused import

--- screens/MainTodo.js
+++ screens/MainTodo.js
@@ -1,6 +1,6 @@
+import React from 'react';
 import { Card } from '../components/Card';
 import { TodoContext } from '../context/TodoContext';
-import React, { useContext } from 'react';
 
 export const MainTodo = ({ data }) => {
   return (
