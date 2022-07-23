98222b550b11cd64126765f41775359b7c0c043f
mingthean.lay <mingthean.lay@gmail.com>
2022-07-23 13:11:58 +0700

    fix: add try catch

--- components/DataProvider.tsx
+++ components/DataProvider.tsx
@@ -27,35 +27,48 @@ export const DataProvider: React.FC<any> = props => {
       isCompleted: false,
       createdAt: new Date().toISOString()
     };
-    // Check if todo is already in the list
-    todos.find(x => x.todo === todo)
-      ? alert('Todo is already in the list')
-      : axios
-          .post(URL, newTodo)
-          .then(() => {
-            setTodos([...todos, newTodo]);
-          })
-          .catch(err => console.log(err));
+
+    try {
+      todos.find(x => x.todo === todo)
+        ? alert('Todo is already in the list')
+        : axios
+            .post(URL, newTodo)
+            .then(() => {
+              setTodos([...todos, newTodo]);
+            })
+            .catch(err => {
+              throw new Error(err);
+            });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   function completeTodo(id: string) {
     const updatedTodo = todos.find(x => x.id === parseInt(id));
-    // PUT todo to server
-    axios
-      .put(`${URL}/${id}`, {
-        todo: updatedTodo.todo,
-        isCompleted: !updatedTodo.isCompleted
-      })
-      .then(() => {
-        const newTodo = todos.map(x => {
-          if (x.id === parseInt(id)) {
-            x.isCompleted = !updatedTodo.isCompleted;
-          }
-          return x;
+
+    try {
+      axios
+        .put(`${URL}/${id}`, {
+          ...updatedTodo,
+          isCompleted: !updatedTodo.isCompleted
+        })
+        .then(() => {
+          const newTodo = todos.map(x => {
+            if (x.id === parseInt(id)) {
+              x.isCompleted = !updatedTodo.isCompleted;
+            }
+            return x;
+          });
+          console.log(newTodo);
+          setTodos(newTodo);
+        })
+        .catch(err => {
+          throw new Error(err);
         });
-        setTodos(newTodo);
-      })
-      .catch(err => console.log(err));
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
